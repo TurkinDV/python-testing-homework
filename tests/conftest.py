4dba5579c7af7287057b668cb9e33dd2da59a967
dturkin <dturkin@nic.ru>
2023-09-16 23:29:49 +0300

    add test on update user view

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,10 +5,17 @@ It may be also used for extending doctest's context:
 1. https://docs.python.org/3/library/doctest.html
 2. https://docs.pytest.org/en/latest/doctest.html
 """
+from typing import TYPE_CHECKING
+
 import pytest
 
 from server.apps.pictures.container import container
 from server.common.django.types import Settings
+
+if TYPE_CHECKING:
+    from django.test import Client
+
+    from tests.plugins.identity.user_factory import UserFactory
 
 pytest_plugins = [
     # Should be the first custom one:
@@ -24,3 +31,10 @@ pytest_plugins = [
 def settings() -> Settings:
     """Django settings."""
     return container.resolve(Settings)
+
+
+@pytest.fixture()
+def authorized_user(client: 'Client', user: 'UserFactory') -> 'UserFactory':
+    """Authorized_user user."""
+    client.force_login(user)
+    return user
